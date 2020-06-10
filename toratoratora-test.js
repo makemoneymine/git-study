784c5f57d828c691af9a80a1d575113ebed380b0
makemoneymine <65711965+makemoneymine@users.noreply.github.com>
2020-06-10 18:15:09 +0900

    Delete toratoratora-test.js

--- toratoratora-test.js
+++
@@ -1,7 +0,0 @@
-var header = document.getElementById('header2');
-var degree = 0;
-function rotateHeader(){
-    degree = degree + 6;
-    header.style.transform = 'rotateX(' + degree + 'deg)';
-}
-setInterval(rotateHeader,20);
